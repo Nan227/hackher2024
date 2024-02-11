085873237582d80bf765d0129e962ec931a064c6
Piyaporn Puangprasert <dngooddeals@gmail.com>
2024-02-11 11:13:03 -0500

    add balance

--- app.js
+++ app.js
@@ -18,7 +18,27 @@ class UI {
   }
   // submit budget method
   submitBudgetForm(){
-    console.log('es 6 works!')
+    const value = this.budgetInput.value;
+    if(value === ""|| value <0 ){
+      this.budgetFeedback.classList.add('showItem');
+      this.budgetFeedback.innerHTML =('<p> value cannot be empty or negative</p>');
+      const self = this;
+      console.log(this);
+
+      setTimeout(function(){
+        self.budgetFeedback.clssList.remove("showItem");
+      },4000);
+
+    }
+    else{
+      this.budgetAmount.textContent = value;
+      this.budgetInput.value = '';
+      this.showBalance();
+    }
+  }
+  //Show labance
+  showBalance(){
+    console.log('xxxx');
   }
 }
 function eventListeners(){
@@ -27,7 +47,7 @@ const expenseForm = document.getElementById('expense-form');
 const expenseList = document.getElementById('expense-list');
 
 //new instance of UI CLASS
-const ui = newUI();
+const ui = new UI();
 
 // budget form submit
 budgetForm.addEventListener('submit', function(event){
